5357ee0c465ef8c666ab513ddb3bfce2a84c32d4
Mareike <mareike.haug@web.de>
2022-05-08 00:16:54 +0200

    draw board

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -3,7 +3,7 @@ import gameService from "../../services/gameService";
 import socketService from "../../services/socketService";
 import {Room, useStore} from "../../store/store";
 
-const boardSize = 100;
+const boardSize = 400;
 
 interface GameProps {
 }
@@ -38,32 +38,60 @@ export function Game({ }: GameProps): JSX.Element {
     }
   };
 
-  useEffect(()=> {
-    if (context) {
-      console.log(context);
+  const drawBoardLines = (context: CanvasRenderingContext2D) => {
       context.strokeStyle = '#000';
-      context.lineWidth   = 2;
+      context.lineWidth = 8;
       context.strokeRect(0, 0, boardSize, boardSize);
+      context.lineWidth = 4;
       context.beginPath();
-
       context.moveTo(0, 0);
       context.lineTo(boardSize, boardSize);
       context.moveTo(boardSize, 0);
       context.lineTo(0, boardSize);
-      context.moveTo((boardSize/2),0);
-      context.lineTo((boardSize/2),boardSize);
-      context.moveTo(0,(boardSize/2));
-      context.lineTo(boardSize,(boardSize/2));
+      context.moveTo((boardSize / 2), 0);
+      context.lineTo((boardSize / 2), boardSize);
+      context.moveTo(0, (boardSize / 2));
+      context.lineTo(boardSize, (boardSize / 2));
+      context.closePath();
       context.stroke();
+  }
+
+  const drawCircle = (context: CanvasRenderingContext2D, x: number, y: number, radius: number) => {
+      context.fillStyle = '#9cadce ';
+      context.beginPath();
+      context.arc(x, y, radius, 0, Math.PI *2);
+      context.closePath();
+      context.fill();
+  }
+
+  useEffect(()=> {
+    if (context) {
+      drawBoardLines(context);
+      drawCircle(context, 0,0,10);
+      drawCircle(context, 0, boardSize,10);
+      drawCircle(context, boardSize,0,10);
+      drawCircle(context, boardSize,boardSize,10);
+      drawCircle(context, boardSize/2,boardSize/2,10);
+      drawCircle(context, boardSize/2,0,10);
+      drawCircle(context, 0,boardSize/2,10);
+      drawCircle(context, boardSize,boardSize/2,10);
+      drawCircle(context, boardSize/2,boardSize,10);
     }
   },[context]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas?.getContext('2d');
-    if(context)  {
-      setContext(context);
+    if (canvas) {
+      canvas.style.width = "400px";
+      canvas.style.height = "400px";
+      const dpi = window.devicePixelRatio;
+      const context = canvas.getContext('2d');
+      if (context)  {
+        context.scale(dpi,dpi);
+        setContext(context);
+      }
     }
+
     if (!listenersAttached) {
       handlePlayerLeft();
       handleGameEnd();
@@ -72,6 +100,6 @@ export function Game({ }: GameProps): JSX.Element {
   }, []);
 
   return (
-    <canvas ref={canvasRef} width="300" height="300"></canvas>
+    <canvas ref={canvasRef} width="800" height="800"></canvas>
   );
 }
