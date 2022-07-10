d722c8eaab2b439926af879220f6203521cae9fb
Mareike <mareike.haug@web.de>
2022-07-10 14:54:29 +0200

    change to hash router

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
