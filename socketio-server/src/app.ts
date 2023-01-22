6f228a8d85152092f76e50792adf61e72c81ba98
Mareike <mareike.haug@web.de>
2023-01-23 00:14:47 +0100

    update dependencies

--- socketio-server/src/app.ts
+++ socketio-server/src/app.ts
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const ensureSecure = require('./middlewares/ensureSecure');
-require ('newrelic');
 
 import "reflect-metadata";
 
